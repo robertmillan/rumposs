92c1e7cb3a7b38746b68e8f68031d4b4295a2ef6
Robert Millan <rmh@freebsd.org>
2015-06-07 13:18:24 +0200

    Request PCI device association to Linux UIO

--- rumposs.sh
+++ rumposs.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
 cmd="$1"
 shift
+
+case $(uname -s) in
+	Linux)
+		echo 64 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
+
+		modprobe uio_pci_generic
+
+		## auich
+		echo "8086 2415" > /sys/bus/pci/drivers/uio_pci_generic/new_id
+
+		## hdaudio
+		echo "8086 2668" > /sys/bus/pci/drivers/uio_pci_generic/new_id
+		echo "8086 27d8" > /sys/bus/pci/drivers/uio_pci_generic/new_id
+	;;
+esac
+
 LD_PRELOAD=./rumposs.so exec $cmd $@
